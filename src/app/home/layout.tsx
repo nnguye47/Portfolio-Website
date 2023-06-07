e64a8ad7c67cb544c155e76d07c909f33d66d772
Nam Nguyen <nam.nguyen201@gmail.com>
2023-06-07 15:03:30 -0700

    Basic styling for the landing page

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -7,14 +7,24 @@ export default function DashboardLayout({
     <section>
       {/* Include shared UI here e.g. a header or sidebar */}
       <nav className="border-blue-600 border-solid border-[5px] bg-teal-300">
-        <p>logo</p>
-        <ul>
-          <li>
-            <a href="#">Home</a>
+        <ul className="flex flex-row justify-around">
+          <li className="justify-self-center">
+            <a href="/">
+              <img src="" alt="logo" />
+            </a>
           </li>
-          <li>
-            <a href="#">About</a>
-          </li>
+          <div className="flex flex-row justify-evenly gap-[50px]">
+            <li>
+              <a href="#" className="text-2xl underline">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-2xl underline">
+                About
+              </a>
+            </li>
+          </div>
         </ul>
       </nav>
       {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import React from "react";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <a href="/home">Continue</a>
+    <main className="">
+      <div className="flex flex-col justify-center items-center w-screen h-screen gap-[50px]">
+        <h1 className="text-5xl font-bold underline ">Welcome To My Website</h1>
+        <a href="/home">Continue</a>
+      </div>
     </main>
   );
 }
