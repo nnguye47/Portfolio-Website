49c47a7aa960f0ff8e1da195bbcf7b40f46f11f7
Nam Nguyen <nam.nguyen201@gmail.com>
2023-06-12 15:43:17 -0700

    create projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import React from "react";
 import Intro from "./components/Intro";
 import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
 
 export default function Home() {
   return (
     <main className="relative">
       <Intro />
       <About />
+      <Projects />
     </main>
   );
 }
