f4e1b73ae5bfa07b3b6830f27d471ebcbd19f319
Nam Nguyen <nam.nguyen201@gmail.com>
2023-06-20 16:49:34 -0700

    define basic layout of a project card

--- src/app/components/Projects/Card.tsx
+++ src/app/components/Projects/Card.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Card() {
-  return <div>individual project</div>;
+  return (
+    <div>
+      <Image src="" alt="exImg" />
+      <p>Project Name</p>
+      <p>Role</p>
+      <p>Description</p>
+    </div>
+  );
 }
