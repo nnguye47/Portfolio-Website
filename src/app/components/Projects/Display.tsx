7004d4cac3dd4e0c6de08c9e2d9a3a478c883824
Nam Nguyen <nam.nguyen201@gmail.com>
2023-06-26 16:08:04 -0700

    define the layout of the display card

--- src/app/components/Projects/Display.tsx
+++ src/app/components/Projects/Display.tsx
@@ -16,11 +16,22 @@ export default function Display({
   Stack,
 }: projectProps) {
   return (
-    <div className="w-[40%] h-[90%] flex-none rounded-lg shadow-inner shadow-cyan-500/50">
-      <h1>My Display of {Title}</h1>
-      <h2>{Role}</h2>
-      <h3>{Description}</h3>
-      <p>Made with {Stack} </p>
+    <div className="w-[40%] h-[90%] flex flex-col justify-start flex-none rounded-lg shadow-inner shadow-cyan-500/50">
+      <span className="h-[40%]">image goes here</span>
+      <div className="flex flex-col justify-start w-[90%] mx-auto">
+        <div className="flex flex-col justify-evenly">
+          <h1 className="text-3xl font-bold">My Display of {Title}</h1>
+          <h2>{Role}</h2>
+          <p>Made with {Stack} </p>
+        </div>
+        <span className="mt-[30px]">
+          {Description}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </span>
+      </div>
     </div>
   );
 }
