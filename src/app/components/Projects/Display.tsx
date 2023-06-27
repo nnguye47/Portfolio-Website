ad3acd5d2a0f74d42bae308167e85835b34a0ebd
Nam Nguyen <nam.nguyen201@gmail.com>
2023-06-27 11:27:22 -0700

    change the display card to use MUI's card

--- src/app/components/Projects/Display.tsx
+++ src/app/components/Projects/Display.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 
 type projectProps = {
   Title: string;
@@ -16,22 +21,47 @@ export default function Display({
   Stack,
 }: projectProps) {
   return (
-    <div className="w-[40%] h-[90%] flex flex-col justify-start flex-none rounded-lg shadow-inner shadow-cyan-500/50">
-      <span className="h-[40%]">image goes here</span>
-      <div className="flex flex-col justify-start w-[90%] mx-auto">
-        <div className="flex flex-col justify-evenly">
-          <h1 className="text-3xl font-bold">My Display of {Title}</h1>
-          <h2>{Role}</h2>
-          <p>Made with {Stack} </p>
-        </div>
-        <span className="mt-[30px]">
-          {Description}
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </span>
-      </div>
-    </div>
+    <Card className="bg-[#031632] h-[100%] w-[40%]">
+      <CardActionArea className="h-[100%]">
+        <CardMedia
+          component="img"
+          className="h-[40%]"
+          image={ImageURL}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h3" component="div" color="#34bac2">
+            {Title}
+          </Typography>
+          <Typography variant="h5" color="#34bac2">
+            {Role}
+          </Typography>
+          <Typography variant="body2" color="#34bac2">
+            {Description}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    // <div className="w-[40%] h-[90%] flex flex-col justify-start flex-none rounded-lg shadow-inner shadow-cyan-500/50">
+    //   <span className="h-[40%]">image goes here</span>
+    //   <div className="flex flex-col justify-start w-[90%] mx-auto">
+    //     <div className="flex flex-col justify-evenly">
+    //       <h1 className="text-3xl font-bold">My Display of {Title}</h1>
+    //       <h2>{Role}</h2>
+    //       <p>Made with {Stack} </p>
+    //     </div>
+    //     <span className="mt-[30px]">
+    //       {Description}
+    //       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    //       eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+    //       minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+    //       aliquip ex ea commodo consequat.
+    //     </span>
+    //   </div>
+    // </div>
   );
 }
